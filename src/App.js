2bba65de7853c0e55d0379b8cc4cabc12bf8a71a
travis <trav.horn@gmail.com>
2020-11-23 21:18:54 -0700

    Bootstrap & initial components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 import Task from './components/Task';
 import AddTask from './components/AddTask';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Task />
-        <AddTask />
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<header className="App-header">
+				Tasks.js
+			</header>
+			<Container className="th-container">
+				<Row>
+					<Col lg>
+						<div>
+							Calendar
+						</div>
+					</Col>
+					<Col>
+						<Task />	
+						<AddTask />
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
