da841d4cfae556d3d317de72c4859dd75eca5d08
travis <travis@travishowell.net>
2020-12-14 20:18:00 -0700

    Improved month/year changing

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,64 +1,97 @@
 import React, { useState } from 'react';
 import { DateTime } from 'luxon';
+import './Calendar.css';
 
 const Calendar = (props) => {
-    const weekdays = [0, 1, 2, 3, 4, 5, 6];
+    // const weekdays = [0, 1, 2, 3, 4, 5, 6];
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    // const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     const dt = DateTime.local();
-    const today = dt.toLocaleString();
+    // const today = dt.toLocaleString();
 
     const [selectedDay, setSelectedDay] = useState(dt);
 
-    const firstDayInMonth = selectedDay.set({day: 1}).weekday;
+    console.log('current selected...', selectedDay, selectedDay.year)
+
+    let firstWeekInMonth = selectedDay.set({ day: 1 }).weekNumber;
+    
+    if (firstWeekInMonth === 53) firstWeekInMonth = 0;
+
     const daysInMonth = selectedDay.daysInMonth;
-    const daysAfterFirstWeek = 7 - daysInMonth;
 
-    // const dateSessions = props.data[selectedDate];
-    // const startWeek = moment(selectedDate).startOf('month').week();
-    // const endWeek = moment(selectedDate).month() != 11 ? moment(selectedDate).endOf('month').week() : 53;
+    let lastWeekInMonth = selectedDay.set({ day: daysInMonth }).weekNumber;
+    if (lastWeekInMonth === 1) lastWeekInMonth = 53;
 
-    /* let calendar = []
-    for (var week = startWeek; week <= endWeek; week++) {
+    let calendar = [];
+    console.log('building calendar', firstWeekInMonth, lastWeekInMonth)
+    for (let week = firstWeekInMonth; week <= lastWeekInMonth; week++) {
+        let useWeek = selectedDay.set({ weekNumber: week });
+        useWeek = useWeek.startOf('week')
+
         calendar.push({
             week: week,
-            days: Array(7).fill(0).map((n, i) => moment(selectedDate).week(week).startOf('week').clone().add(n + i, 'day'))
-        })
-    } */
+            days: Array(7).fill(0).map((n, i) => useWeek.set({ day: useWeek.day + i - 1})) 
+        });
+
+        if (week === lastWeekInMonth && useWeek.endOf('week').day === daysInMonth) {
+            let lastWeek = useWeek.set({weekNumber: week + 1});
+
+            calendar.push({
+                week: week + 1,
+                days: Array(7).fill(0).map((n, i) => lastWeek.set({ day: lastWeek.day + i - 1 })) 
+            });
+        }
+    }
+
+    console.log(calendar)
 
     const abbreviate = (str) => str.substring(0, 3);
-    
-    console.log(dt, firstDayInMonth, daysInMonth);
+
+    console.log(selectedDay);
     return (
-        <div>
-            <table className="table">
-                <thead>
-                    <tr>
-                        {days.map((d, index) => (
-                            <th key={index}>{abbreviate(d)}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* First week in month */}
-                    <tr>
-                        {weekdays.map((x, index) => (
-                            <td key={index}>
-                                {x === firstDayInMonth ? 1 : (
-                                    x > firstDayInMonth && x - firstDayInMonth + 1
-                                )}
-                            </td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td scope="row"></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-            Calendar
+        <div className="calendar">
+            <div className="calendar-header">
+                <div className="calendar-month-btn" onClick={(() => {
+                    setSelectedDay(selectedDay.set({month: selectedDay.month - 1}));
+                })}>
+                    {/* <FaAngleLeft /> */}
+                    Prev
+                </div>
+                <div className="calendar-header-month">
+                    {selectedDay.monthLong}{selectedDay.year !== dt.year && <span>&nbsp;{selectedDay.year}</span>}
+                </div>
+                <div className="calendar-month-btn" onClick={(() => {
+                    setSelectedDay(selectedDay.set({month: selectedDay.month + 1}));
+                })}>
+                    {/* <FaAngleRight /> */}
+                    Next
+                </div>
+            </div>
+
+            <div className="calendar-btn-container">
+                <div className="calendar-today-btn" onClick={(()=> setSelectedDay(dt))}>
+                    Today
+                </div>
+            </div>
+
+
+            <div className="calendar-week calendar-week-header">
+                {days.map((d, index) => (
+                    <div key={index}>{abbreviate(d)}</div>
+                ))}
+            </div>
+            {calendar.map((week) => (
+                <div key={week.week} className="calendar-week">
+                    {week.days.map((day, index) => (
+                        <div key={index} /* className={dateClassName(day)} */>
+                            <div className={''/* dayClassName(day) */} onClick={(() => setSelectedDay(day))}>
+                                {day.day}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div>
     )
 }
